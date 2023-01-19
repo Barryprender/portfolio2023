4d7db82c7a1bd7939a9ed08f8443f5188bc4b952
BarryPRE_78 <barryprendergast78@gmail.com>
2023-01-19 18:39:46 +0100

    add pages to routing

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
-import { PagesComponent } from './pages.component';
+import { SkillsComponent } from './skills/skills.component';
 
-const routes: Routes = [{ path: 'home', component: HomeComponent }];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'contact', component: ContactComponent }
+];
+
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
+
 export class PagesRoutingModule { }
