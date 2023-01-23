691275c3ccbbee005d646da71684a3a2ea9e33c4
BarryPRE_78 <barryprendergast78@gmail.com>
2023-01-23 20:08:54 +0100

    add spanish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -27,7 +27,7 @@ import { DatePipe } from '@angular/common';
     NgbModule,
     NgbNavModule
   ],
-  providers: [PortfolioService, DatePipe],
+  providers: [PortfolioService, DatePipe,{ provide: LOCALE_ID, useValue: 'es' },],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+registerLocaleData(localeEs);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
