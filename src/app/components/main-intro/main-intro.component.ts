1e5af3c4e47224c0c4c97e6df8ab1fc80225283e
BarryPRE_78 <barryprendergast78@gmail.com>
2023-01-24 19:29:51 +0100

    create mainIntro component

---
+++ src/app/components/main-intro/main-intro.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-intro',
+  templateUrl: './main-intro.component.html',
+  styleUrls: ['./main-intro.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MainIntroComponent {
+
+}
