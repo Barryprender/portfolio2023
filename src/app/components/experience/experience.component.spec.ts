a247d5a5449f0e5b9870aeb5e6ea2ecab5072700
BarryPRE_78 <barryprendergast78@gmail.com>
2023-04-04 23:07:53 +0200

    add unit test for experieces component

--- src/app/components/experience/experience.component.spec.ts
+++ src/app/components/experience/experience.component.spec.ts
@@ -1,23 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ExperienceComponent } from './experience.component';
+import { ExperienceModel } from 'src/app/models/experienceModel.model';
+import { By } from '@angular/platform-browser';
+import { ExperienceService } from 'src/app/services/experience-service.service';
+import { BehaviorSubject } from 'rxjs';
 
 describe('ExperienceComponent', () => {
   let component: ExperienceComponent;
   let fixture: ComponentFixture<ExperienceComponent>;
+  let experienceService: ExperienceService;
 
   beforeEach(async () => {
+    // stub ExperienceService to provide test data
+    const experienceStub = {
+      currentListExperienceData: new BehaviorSubject<Array<ExperienceModel>>([
+        // provide sample data for testing
+        {  title: 'Test Experience 1',
+            description: 'This is test experience 1',
+            link: 'example.com',
+            finishDate: '01/11/1220',
+            startDate: '01/11/1220',
+            position: '',
+            skills: ['sdf', 'sdfsad'],
+            actual: false,
+         },
+      ]),
+    };
+
     await TestBed.configureTestingModule({
-      declarations: [ ExperienceComponent ]
+      declarations: [ ExperienceComponent ],
+      providers: [ { provide: ExperienceService, useValue: experienceStub } ]
     })
     .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(ExperienceComponent);
     component = fixture.componentInstance;
+    experienceService = TestBed.inject(ExperienceService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display experiences', () => {
+    const cardElements = fixture.debugElement.queryAll(By.css('.card'));
+    expect(cardElements.length).toEqual(component.experiences.length);
   });
 });
