c2c9f1e34ecab74d47aa456e7c5104e6cb770ad6
BarryPRE_78 <barryprendergast78@gmail.com>
2023-04-21 00:21:07 +0200

    update pages router

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
@@ -14,7 +13,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes), NgbNavModule],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 
