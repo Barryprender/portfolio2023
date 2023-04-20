b6b32c3be49032311f1d1a2ad38f474ed77eba9b
BarryPRE_78 <barryprendergast78@gmail.com>
2023-04-20 12:07:54 +0200

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { SkillsPageComponent } from './pages/skills/skills-page.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent }
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'skills', component: SkillsPageComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes),  NgbNavModule],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes), AppRoutingModule],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
