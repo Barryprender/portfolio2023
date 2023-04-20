85d851a5325df247ecc93af1290b89ef4e49768e
BarryPRE_78 <barryprendergast78@gmail.com>
2023-04-20 12:14:16 +0200

    pass tests

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -3,24 +3,26 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
 import { MatCard } from '@angular/material/card';
 import { MainIntroComponent } from 'src/app/components/main-intro/main-intro.component';
-import { SkillsPageComponent } from '../skills/skills-page.component';
+import { SkillsComponent } from 'src/app/components/skills/skills.component';
+import { Firestore, collectionData } from '@angular/fire/firestore';
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+    let component: HomeComponent;
+    let fixture: ComponentFixture<HomeComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HomeComponent, MatCard, MainIntroComponent, SkillsPageComponent  ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            providers: [{ provide: Firestore, useValue: { collectionData } }],
+            declarations: [HomeComponent, MatCard, MainIntroComponent, SkillsComponent]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(HomeComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
