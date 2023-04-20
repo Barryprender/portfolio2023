860c0ec59a65caf0b47c01b352ae4feb1004898c
BarryPRE_78 <barryprendergast78@gmail.com>
2023-04-20 23:29:06 +0200

    edit delete skills

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -11,17 +11,32 @@ import { NgForm } from "@angular/forms";
 })
 
 export class SkillsComponent {
+    public isfocused: boolean = false;
     // Observable to store the skills data
     public skills!: Observable<any>;
     // Model for the skills data
     public model = [] as unknown as SkillsModel;
+    public toggle: boolean = false;
 
     constructor(
         // Firestore instance
         private firestore: Firestore,
-    ) {
-        // Get the skills data from the database
-        this.getSkills();
+        ) {
+            // Get the skills data from the database
+            this.getSkills();
+        }
+
+        inputFocus(){
+            this.isfocused = true;
+            return this.isfocused;
+        }
+
+    public editDoc(){
+        if(this.toggle){
+            return false;
+        }
+        this.toggle = !this.toggle;
+        return this.toggle;
     }
 
     // Add a skill to the database
