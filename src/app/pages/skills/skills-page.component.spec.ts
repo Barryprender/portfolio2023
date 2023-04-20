ca41a2e6b571d931a14103d8ec2ed4e61287a737
BarryPRE_78 <barryprendergast78@gmail.com>
2023-04-20 19:05:18 +0200

    update skills page and test

--- src/app/pages/skills/skills-page.component.spec.ts
+++ src/app/pages/skills/skills-page.component.spec.ts
@@ -1,24 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SkillsPageComponent } from './skills-page.component';
 import { MatCard } from '@angular/material/card';
+import { SkillsComponent } from 'src/app/components/skills/skills.component';
 
-describe('SkillsComponent', () => {
-  let component: SkillsPageComponent;
-  let fixture: ComponentFixture<SkillsPageComponent>;
+describe('SkillsPagesComponent', () => {
+    let component: SkillsPageComponent;
+    let fixture: ComponentFixture<SkillsPageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SkillsPageComponent, MatCard ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [SkillsPageComponent, MatCard, SkillsComponent]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(SkillsPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(SkillsPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
 });
