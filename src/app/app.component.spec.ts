1a31d5e3e0d92b26f3c3f0ee4155f0efd235cd71
BarryPRE_78 <barryprendergast78@gmail.com>
2023-04-20 12:13:14 +0200

    pass tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,24 +1,38 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
+
+class ActivatedRouteStub {
+    snapshot = {
+        paramMap: {
+            get(): string {
+                return 'example_value';
+            }
+        }
+    };
+}
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent,
-        HeaderComponent,
-      ],
-    }).compileComponents();
-  });
+    let component: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [RouterOutlet],
+            declarations: [AppComponent, HeaderComponent, NavComponent],
+            providers: [{ provide: ActivatedRoute, useClass: ActivatedRouteStub }],
+        })
+            .compileComponents();
+    });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    })
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
