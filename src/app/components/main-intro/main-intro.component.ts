eef3e49d699ff67ae6ed8f2473189692ba9a1d4d
BarryPRE_78 <barryprendergast78@gmail.com>
2023-05-01 23:20:45 +0200

    ts file

--- src/app/components/main-intro/main-intro.component.ts
+++ src/app/components/main-intro/main-intro.component.ts
@@ -1,11 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-main-intro',
-  templateUrl: './main-intro.component.html',
-  styleUrls: ['./main-intro.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-main-intro',
+    templateUrl: './main-intro.component.html',
+    styleUrls: ['./main-intro.component.scss'],
+    // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MainIntroComponent {
-
+export class MainIntroComponent implements OnInit {
+   public contentLoaded: boolean = false;
+    ngOnInit(): void {
+        setTimeout(() => {
+            this.contentLoaded = true;
+        }, 1000);
+    }
 }
