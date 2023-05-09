0f7e6e4d413bd1f38b6017b407c814c1cc445d26
BarryPRE_78 <barryprendergast78@gmail.com>
2023-05-09 19:35:33 +0200

    add and implement breakpoint observer for controling nav

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router'
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout'
 
 @Component({
     selector: 'app-nav',
@@ -11,22 +12,37 @@ export class NavComponent implements OnInit {
     public navOpen = false;
     public activeLink!: string;
     public menuString: string = 'menu';
+    private mobileScreen: boolean = false;
 
     constructor(
+        private breakpointObserver: BreakpointObserver,
         public router: Router,
         public route: ActivatedRoute,
-    ) { }
+    ) {
+        // detect screen size changes
+        this.breakpointObserver.observe([
+            "(max-width: 768px)"
+        ]).subscribe((result: BreakpointState) => {
+            if (result.matches) {
+                this.mobileScreen = true;
+            } else {
+                this.mobileScreen = false;
+            }
+        });
+    }
+
+    toggleMobileNav() {
+        if(this.mobileScreen) {
+            this.navOpen = !this.navOpen;
+            if (this.menuString === 'menu_open') {
+                this.menuString = 'menu'
+            } else {
+                this.menuString = 'menu_open';
+            }
+        }
+    }
 
     ngOnInit(): void {
     }
 
-    toggleMobileNav(){
-        this.navOpen = !this.navOpen;
-
-        if (this.menuString === 'menu_open'){
-            this.menuString = 'menu'
-        } else {
-            this.menuString = 'menu_open';
-        }
-    }
 }
