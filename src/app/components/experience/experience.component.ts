cb83952be74e862af605f4aa78e45acff579e2ad
BarryPRE_78 <barryprendergast78@gmail.com>
2023-08-29 17:54:34 +0200

    experiences working

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,21 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore, collection, addDoc, collectionData, doc, updateDoc, deleteDoc, DocumentData } from '@angular/fire/firestore';
+import { BehaviorSubject } from 'rxjs';
 import { ExperienceModel } from 'src/app/models/experienceModel.model';
 import { ExperienceService } from 'src/app/services/experience-service.service';
 
 @Component({
-  selector: 'app-experience',
-  templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.scss']
+    selector: 'app-experience',
+    templateUrl: './experience.component.html',
+    styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent {
     public experiences = new Array<ExperienceModel>();
     public model = ExperienceModel;
+    public workExperience$: BehaviorSubject<ExperienceModel[]> = new BehaviorSubject<ExperienceModel[]>([]);
+    public shortDesc: string = '';
 
     constructor(
         private experienceService: ExperienceService,
-      ){
-        this.experienceService.currentListExperienceData.subscribe(data => {
-          this.experiences = data;
-        })
-      }
+        private firestore: Firestore = inject(Firestore),
+    ) {
+        // this.experienceService.currentListExperienceData.subscribe(data => {
+        //     this.experiences = data;
+        // })
+        this.getWorkExperience();
+    }
+
+    // public getWorkExperience() {
+    //     const collectionInstance = collection(this.firestore, 'WorkExperience');
+    //     collectionData(collectionInstance, { idField: 'id' })
+    //         .subscribe((WorkExperience: DocumentData[]) => {
+    //             const WorkExperienceArray = WorkExperience.map(experience => experience as ExperienceModel);
+    //             this.workExperience$.next(WorkExperienceArray);
+    //         })
+    // }
+
+    public getWorkExperience() {
+        const collectionInstance = collection(this.firestore, 'WorkExperience');
+        collectionData(collectionInstance, { idField: 'id' })
+            .subscribe((experiences: DocumentData[]) => {
+                const experiencesArray = experiences.map(experience => experience as ExperienceModel);
+                this.workExperience$.next(experiencesArray);
+
+                experiencesArray.forEach(experience => {
+                    if (experience.description) {
+                        const sentences = experience.description.split(/(?<=\.)\s/);
+                        this.shortDesc = sentences[0];
+                    }
+                });
+            })
+    }
 }
