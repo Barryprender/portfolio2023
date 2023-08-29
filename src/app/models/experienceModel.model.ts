1cc7a1c1771d8d21228698dbf77480e2085dc5ad
BarryPRE_78 <barryprendergast78@gmail.com>
2023-08-29 17:54:50 +0200

    update experience model

--- src/app/models/experienceModel.model.ts
+++ src/app/models/experienceModel.model.ts
@@ -5,19 +5,19 @@ export class ExperienceModel {
     link : string;
     actual? : boolean = false;
     startDate : string;
-    finishDate : string;
+    endDate : string;
     position: string;
     skills : string[];
     description : string;
     examples? : ExamplesModel[];
 
-    constructor (title: string, link: string, actual: boolean, startDate: string, finishDate: string, position: string,skills: string[], description: string, examples?: ExamplesModel[]
+    constructor (title: string, link: string, actual: boolean, startDate: string, endDate: string, position: string,skills: string[], description: string, examples?: ExamplesModel[]
     ) {
         this.title = title;
         this.link = link;
         this.actual = actual;
         this.startDate = startDate;
-        this.finishDate = finishDate;
+        this.endDate = endDate;
         this.position = position;
         this.skills = skills;
         this.description = description;
