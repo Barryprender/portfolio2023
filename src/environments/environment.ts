fca1590d08173d394608d9f02d04231787e2197d
BarryPRE_78 <barryprendergast78@gmail.com>
2023-08-29 17:54:19 +0200

    update enviornment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
-  firebase: {
-    projectId: 'portfoliodata2023',
-    appId: '1:558406061036:web:e768d0cd7471f4d38a4ec9',
-    storageBucket: 'portfoliodata2023.appspot.com',
-    apiKey: 'AIzaSyAQrjWbfD1Gjwqb0NfWevec11SYHdWbjsc',
-    authDomain: 'portfoliodata2023.firebaseapp.com',
-    messagingSenderId: '558406061036',
-    measurementId: 'G-HP3DHXSWNF',
-  },
+    firebase: {
+        apiKey: "AIzaSyAQrjWbfD1Gjwqb0NfWevec11SYHdWbjsc",
+        authDomain: "portfoliodata2023.firebaseapp.com",
+        projectId: "portfoliodata2023",
+        storageBucket: "portfoliodata2023.appspot.com",
+        messagingSenderId: "558406061036",
+        appId: "1:558406061036:web:e768d0cd7471f4d38a4ec9",
+        measurementId: "G-HP3DHXSWNF"
+    },
     production: false
 }
