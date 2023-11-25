eafbf143fac60563cd61aa2b2eb99f82a1e1cacb
BarryPRE_78 <barryprendergast78@gmail.com>
2023-11-25 21:06:02 +0100

    add experience to skills

--- src/app/models/skillsModel.model.ts
+++ src/app/models/skillsModel.model.ts
@@ -2,14 +2,16 @@ export class SkillsModel {
     id = '';
     skill = '';
     description? = '';
-
+    experience? = '';
     constructor(
         id: string,
         skill: string,
         description?: string,
+        experience?: string,
     ) {
         this.id = id;
         this.skill = skill;
         this.description = description
+        this.experience = experience
     }
 }
